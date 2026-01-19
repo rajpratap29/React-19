d3b9366eab5e119f7ad7dc2c92e5653848f6525d
rajpratap29 <therajpratapofficial@gmail.com>
2026-01-19 15:23:26 +0530

    Update: Basic Project Foundation

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper"></div>
+      <header>
+        <h1>
+          Find <span className="text-gradient">Movies</span> You'll Enjoy
+          Without the Hassle
+        </h1>  
+      </header>
+
+      <p>Search</p>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    </main>
+  );
+};
+
+export default App;
